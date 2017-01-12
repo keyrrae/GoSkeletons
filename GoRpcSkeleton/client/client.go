daed7c38986a4b59399c8f0eef5eeed3dede4e05
keyrrae <xuan-wang@outlook.com>
2017-01-11 20:54:41 -0800

    refactor directory

---
+++ GoRpcSkeleton/client/client.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+	"os"
+	"time"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "server")
+		os.Exit(1)
+	}
+	serverAddress := os.Args[1]
+	
+	client, err := rpc.DialHTTP("tcp", serverAddress+":1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	
+	
+	for{
+		// Synchronous call
+		args := Args{17, 8}
+		var reply int
+		err = client.Call("Arith.Multiply", args, &reply)
+		if err != nil {
+			log.Fatal("arith error:", err)
+		}
+		fmt.Printf("Arith: %d*%d=%d\n", args.A, args.B, reply)
+		
+		var quot Quotient
+		err = client.Call("Arith.Divide", args, &quot)
+		if err != nil {
+			log.Fatal("arith error:", err)
+		}
+		fmt.Printf("Arith: %d/%d=%d remainder %d\n", args.A, args.B, quot.Quo, quot.Rem)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
